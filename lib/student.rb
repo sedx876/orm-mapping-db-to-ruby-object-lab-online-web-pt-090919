ff59bc386dc15b15c664355a46b4a4aacc3ce75d
Sharon Watkins <swatkins876@gmail.com>
2019-11-11 18:19:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,18 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
